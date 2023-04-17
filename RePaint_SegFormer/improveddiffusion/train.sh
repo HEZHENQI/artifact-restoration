b8d8da5066159c4ddbb45088ee97a905e73b761d
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-04-17 12:57:50 +0800

    Delete RePaint_SegFormer directory

--- RePaint_SegFormer/improveddiffusion/train.sh
+++
@@ -1,6 +0,0 @@
-export OPENAI_LOGDIR="/root/autodl-tmp/models_diffusion_SegFormer"
-
-MODEL_FLAGS="--image_size 256 --num_channels 128 --num_res_blocks 3 --learn_sigma True"
-DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
-TRAIN_FLAGS="--lr 1e-4 --batch_size 8"
-python scripts/image_train.py --data_dir /root/autodl-tmp/AACycleDataset/trainB $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
